893f2692d411b97b804b62576a380043a5c03212
anbu <anbuselvam.sk05@gmail.com>
2025-09-13 20:46:14 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import profilePic from './assets/anbu.jpg';
 import projectpic from './assets/image1.png';
 import projectpic1 from './assets/img.png';
 import upcoming from './assets/image.jpeg';
-import resume from './assets/resume.pdf';
+import resume from './assets/Resume.pdf';
 import idea from './assets/ideathon.jpg';
 import gaming from './assets/gaming.png';
 import Typewriter from "typewriter-effect"
@@ -32,7 +32,7 @@ function App() {
         <a href="#project">Project</a>
         <a href="#ac">Achievement</a>
         <a href="#con">Contact Me</a>
-        <a href={resume} download="resume.pdf" className='do' > Resume</a>
+        <a href={resume} download="Resume.pdf" className='do' > Resume</a>
       </div>
     </div>
 
